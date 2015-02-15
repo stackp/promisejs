285f07fe8032384a2973d89970f7f0fe41189f4e
Yannick Dalbin <yannick@idvive.com>
2015-02-15 20:18:44 +0100

    Add NodeJS compatibility

--- promise.js
+++ promise.js
@@ -204,6 +204,9 @@
             return promise;
         });
     } else {
+        if (typeof module !== "undefined" && module.exports) {
+            exports = module.exports = promise;
+        }
         exports.promise = promise;
     }
 
